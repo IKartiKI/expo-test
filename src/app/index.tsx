aa9184ad2778bb890e0a22db96c279c24cde9947
IKartiKI <ks.kartik1610@gmail.com>
2026-01-09 16:19:08 +0530

    updated index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,39 +10,7 @@ export default function IndexScreen() {
       <AppText center size = "heading" bold>
         Index Screen
       </AppText>
-      {/* <Link href="/second" push asChild>
-        <Button title = "Push to second"/>
-      </Link>
-      <Link href={{
-         pathname: "/second",
-         params: { name: "Kartik"}
-        }} 
-         push asChild>
-        <Button title = "Greet Kartik on /second" theme="secondary"/>
-      </Link>
-        <Button 
-          title="Greet Ansh on /second" 
-          theme="secondary" 
-          onPress={() => {
-              router.push({ pathname: "/second", params: { name: "Ansh"}});
-          }}
-        />
-        <Link href="/proverbs/1" push asChild>
-          <Button title = "Push to /proverbs/1" theme="tertiary"/>
-        </Link>
-        <Link href={{
-         pathname: "/proverbs/[id]",
-         params: { id: "2"}
-        }} 
-         push asChild>
-        <Button title = "Push to /proverbs/2" theme="secondary"/>
-      </Link>
-      <Link href="/products/shoes/1234" push asChild>
-          <Button title = "Push to /products/shoes/1234" theme="tertiary"/>
-        </Link>
-      <Link href="/products/shoes/1234" push asChild>
-          <Button title = "Push to /products/shoes/1234" theme="tertiary"/>
-        </Link> */}
+      
     </View>
   );
 }
