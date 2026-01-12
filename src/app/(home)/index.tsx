388699fcdf4f06a5047fea6f8a65129578dc8f17
IKartiKI <ks.kartik1610@gmail.com>
2026-01-12 11:21:44 +0530

    Alert modal added

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -2,9 +2,31 @@ import { View } from "react-native";
 import { AppText } from "@/components/AppText";
 import { Link, useRouter } from "expo-router";
 import { Button } from "@/components/Button";
+import { Alert } from "react-native";
+
 
 export default function IndexScreen() {
-  const router = useRouter(); 
+  const router = useRouter();
+  const canGoBack = router.canGoBack(); 
+
+  const handleOpenAlert = () => {
+    // Alert.alert(
+    //   "Navigation Info",
+    //   canGoBack ? "You can go back to the previous screen." : "No previous screen to go back to."
+    // );
+    Alert.alert("Warning!", "Are you sure you want to proceed?", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { 
+        text: "OK",
+        onPress: () => console.log("OK Pressed"),
+        style: "destructive"
+     }
+    ]);
+  }
   return (
     <View className="justify-center flex-1 p-4">
       <AppText center size = "heading" bold>
@@ -13,12 +35,19 @@ export default function IndexScreen() {
       <Link href="/home-nested" push asChild>
         <Button title="Push to /home-nested" />
       </Link>
+      {canGoBack ? (
+        <Button 
+          title="Back"
+          theme= "secondary" 
+          onPress={() => {
+            router.back(); 
+          }}
+        />
+      ) : null}
       <Button 
-        title="Back"
-        theme= "secondary" 
-        onPress={() => {
-           router.back(); 
-        }}
+        title="Check Can Go Back"
+        theme="secondary"
+        onPress={handleOpenAlert}
       />
     </View>
   );
